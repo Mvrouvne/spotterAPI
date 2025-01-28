2f9485c46c74610414d0ede73abfc051bd740fa8
Mvrouvne <marouanechaibat@gmail.com>
2025-01-29 00:55:01 +0100

    added fuel costs, fuel stops, 500+ mile map image

--- backend/routesAPI/views.py
+++ backend/routesAPI/views.py
@@ -1,4 +1,10 @@
+import us
+import os
+import math
 import requests
+import pandas as pd
+import reverse_geocoder as rg
+from django.conf import settings
 from rest_framework import status
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -41,26 +47,108 @@ class   RoutesView(APIView):
         print('total_distance ==> ', distance_miles)
 
         MAX_RANGE = 500 # in miles
+        df = pd.read_csv('fuel-prices.csv')
+        milestones = []
+        legs = route_response['routes'][0]['legs']
+        interval_distance = 500 * 1609.34
+        cumulative_distance = 0
+        next_milestone = interval_distance
+        target_states = []
+        cities = ""
+        gallons_needed = distance_miles / 10
+        avg_fuel_cost = df['Retail Price'].mean()
+        total_fuel_cost = gallons_needed * avg_fuel_cost
 
-        if distance_miles < MAX_RANGE:
-            polyline = route_response['routes'][0]['overview_polyline']['points']
-            static_map_url = 'https://maps.googleapis.com/maps/api/staticmap'
-            static_map_response = requests.get(
-                static_map_url,
-                params = {
-                    "size": "600x400",
-                    "path": f"enc:{polyline}",
-                    "markers": f"color:blue|{start_location}",
-                    "markers": f"color:red|{finish_location}",
-                    "key": KEY
-                })
-            
-            # Save the image
-            if static_map_response.status_code == 200:
-                 return HttpResponse(static_map_response.content, content_type="image/png")
-            else:
-                print(f"Error fetching static map: {static_map_response.status_code}")
+        polyline = route_response['routes'][0]['overview_polyline']['points']
+        static_map_url = 'https://maps.googleapis.com/maps/api/staticmap'
+        static_map_response = requests.get(
+            static_map_url,
+            params = {
+                "size": "600x400",
+                "path": f"enc:{polyline}",
+                "markers": f"color:blue|{start_location}",
+                "markers": f"color:red|{finish_location}",
+                "key": KEY
+            })
         
+        # Save the image
+        if static_map_response.status_code == 200:
+            file_path = os.path.join(settings.MEDIA_ROOT, "static_map.png")
+            with open(file_path, "wb") as f:
+                f.write(static_map_response.content)
+
+            # Generate the image URL
+            image_url = request.build_absolute_uri(settings.MEDIA_URL + "static_map.png")
+
+            # Return JSON response with the image URL
+            if distance_miles <= MAX_RANGE:
+                return Response({
+                    "image_url": image_url,
+                    "trip_data": {
+                        "distance_miles": distance_miles,
+                        "fuel_stops": "None",
+                        "total_fuel_cost": f'${total_fuel_cost:.2f}',
+                    }})
+
+            #  return HttpResponse(static_map_response.content, content_type="image/png")
+        else:
+            return Response(f"Error fetching static map: {static_map_response.status_code}", status=status.HTTP_417_EXPECTATION_FAILED)
+
+        for leg in legs:
+            for step in leg['steps']:
+                step_distance = step['distance']['value']
+                cumulative_distance += step_distance
+                
+                # Check if we've reached or passed the next milestone
+                while cumulative_distance >= next_milestone:
+                    lat = step['end_location']['lat']
+                    lng = step['end_location']['lng']
+                    milestones.append([lat, lng])
+
+                    next_milestone += interval_distance
+        
+        for coord in milestones:
+            result = rg.search(coord)
+            target_states.append(result[0]['admin1'])
+
+        # Initialize an empty array to store the results
+        fuel_stops = []
+
+        # Loop through each state
+        for state in target_states:
+            # Filter the rows for that state
+            s = us.states.lookup(state)
+            s = s.abbr
+            df_state = df[df['State'] == s]                
+            # Find the row with the lowest retail price in that state
+            lowest_price_row = df_state.loc[df_state['Retail Price'].idxmin()]
+            
+            # Add relevant info to the fuel_stops array
+            fuel_stops.append({
+                'State': s,
+                'City': lowest_price_row['City'],
+                'Truckstop Name': lowest_price_row['Truckstop Name'],
+                'Address': lowest_price_row['Address'],
+                'Retail Price': lowest_price_row['Retail Price']
+            })
+
+        for stop in fuel_stops:
+            cities += stop['City'].strip() + ', '
+            cities += stop['State'].strip() + ' | Fuel price: $'
+            cities += str(stop['Retail Price'])
+            cities += '+'
+
+        waypoints = cities.split('+')
+
+        return Response({
+            "image_url": image_url,
+            "trip_data": {
+                "distance_miles": distance_miles,
+                "fuel_stops": waypoints,
+                "total_fuel_cost": f'${total_fuel_cost:.2f}',
+            }})
+            
+
         ## import fuel fiel using pandas
 
         return Response({'response': route_response})
